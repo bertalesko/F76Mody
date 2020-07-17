c6618507c5716b1947e44bcb6e03465669325e4e
brtowsky <Kfiusz@gmail.com>
2020-07-17 09:06:34 +0100

    Update Mody.cpp

--- Mody.cpp
+++ Mody.cpp
@@ -11,8 +11,9 @@ HWND okno = NULL;
 
 int wlaczMody()
 {
+	//nazwa pliku ini z modami
 	char oldname[] = "mody.ini";
-	
+	//sciezka do pliku Fallut76Custom.ini - edytowac pod swoj system !
 	char newname[] = "C:\\Users\\kfius\\OneDrive\\Dokumenty\\My games\\Fallout 76\\Fallout76Custom.ini";
 	
 	if (!CopyFileA(oldname, newname, FALSE))
@@ -30,8 +31,9 @@ int wlaczMody()
 
 int wylaczMody()
 {
-
+	//nazwa pliku ini , czystego
 	char oldname[] = "czysty.ini";
+	//sciezka do pliku Fallut76Custom.ini - edytowac pod swoj system !
 	char newname[] = "C:\\Users\\kfius\\OneDrive\\Dokumenty\\My games\\Fallout 76\\Fallout76Custom.ini";
 
 
@@ -89,7 +91,7 @@ bool DoubleKeyToggle(DWORD KeyCodeA, DWORD KeyCodeB, bool* KeyToggle)
 }
 
 bool sprawdz(HWND ok, char* nazwa)
-{
+{ // Ta funkcja sprawdza czy gra jest aktualnie odpalone, zeby nie robic podmiany plikow na dzialajacej grze.
 	if(ok) 
 	{
 		cout << "Wylacz " << nazwa << " aby kontynowac.\nZamykam."<< endl;
@@ -107,24 +109,24 @@ int main()
 	bool OneKeyToggle = false;
 	bool TwoKeyToggle = false;
 	bool ThreeKeyToggle = false;
-	char nazwaOkna[] = "Fallout76";
-	okno = FindWindowA(0, (LPCSTR)(nazwaOkna)); // nazwa okna
+	char nazwaOkna[] = "Fallout76";// nazwa okna
+	okno = FindWindowA(0, (LPCSTR)(nazwaOkna)); 
 
 	cout << "1. Wylacz mody" << endl;
 	cout << "2. Wlacz mody" << endl;
 	cout << "END. aby zakonczyc" << endl;
+	//sprawdza czy okno gry jest aktywne, jak jest to wylacza program.
 	if (sprawdz(okno, nazwaOkna))
 	{
 		return 0;
 	}
 	
+	
+	// Wcisniecie END wylacza prgram
 	while (!GetAsyncKeyState(VK_END))
 	{
 
-
-
-
-
+			//Shift + 1 Wylacza mody
 		if (DoubleKeyToggle(VK_SHIFT, '1', &ThreeKeyToggle))
 		{
 			wylaczMody();
@@ -132,6 +134,7 @@ int main()
 			Sleep(666);
 
 		}
+		//Shift + 2 Wlacza mody
 		if (DoubleKeyToggle(VK_SHIFT, '2', &ThreeKeyToggle))
 		{
 			wlaczMody();
@@ -140,6 +143,8 @@ int main()
 
 		}
 	}
+	
+	//Pozegnanie
 	cout << "Wylaczam" << endl;
 	cin.get();
 	return 0;
